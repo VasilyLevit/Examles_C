4b6c8fe4c60a7a110e3dae09982420c17660ecec
Vasily Levit <vasily.levit@gmail.com>
2022-06-06 23:05:21 +0800

    Add program If else

---
+++ Ex006_if/Program.cs
@@ -0,0 +1,12 @@
+﻿// Применеие оператора ветвления if else
+Console.WriteLine("Введите имя пользователя: ");
+string userName = Console.ReadLine();
+if(userName.ToLower() == "маша")  // To.Lower() переводит вводимые символы в нижний регистр
+{
+    Console.WriteLine("Ура, это же МАША!");
+}
+else
+{
+    Console.Write("Hello, ");
+    Console.WriteLine(userName);
+}
