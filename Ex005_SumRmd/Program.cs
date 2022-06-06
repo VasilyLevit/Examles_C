2d42350c1b93df220b46fc4bc59a08449e1c2a8a
Vasily Levit <vasily.levit@gmail.com>
2022-06-06 22:49:54 +0800

    Add program SumRnd

---
+++ Ex005_SumRmd/Program.cs
@@ -0,0 +1,6 @@
+﻿// Сумма 2х случайных чисел
+int numberA = new Random().Next(1, 10);
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1, 10);
+Console.WriteLine(numberB);
+Console.WriteLine(numberA + numberB);
